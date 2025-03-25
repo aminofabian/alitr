df803ff19ae75e46c8f38e26569429f2999da224
Fabian Amino <aminofab@gmail.com>
2025-03-25 15:56:17 +0300

    added

--- alitr/models.py
+++ alitr/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
--- alitr/views.py
+++ alitr/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 def index(request):
     return render(request, 'home.html')
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, 'todos.html', {'todos': items})
+
